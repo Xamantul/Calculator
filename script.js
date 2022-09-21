6af8636d2391188abf5d5208662407e010771903
Xamantul <68664835+Xamantul@users.noreply.github.com>
2022-09-21 20:32:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ buttons.forEach((item) => {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
     } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
+      try {
+           display.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = "Error!";
+      }
     } else if (display.innerText == "" && item.id == "equal") {
       display.innerText = "Empty!";
       setTimeout(() => (display.innerText = ""), 2000);
